bfa8be72b2998ef7b35807deb8f8acf48c97c96e
Fogo Adepitan <Fogoadepitan100@gmail.com>
2023-12-01 21:59:46 +0100

    route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -9,14 +10,29 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({ email: session.user.email });
+
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
   async signIn({ profile }) {
     try {
       await connectToDB();
 
       //check if the user exists
-
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
       //if not ,create the user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.username.replace("", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
     } catch (err) {}
   },
 });
