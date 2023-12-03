120f7ba01ade86b82e9353cc5fd6af378d4cd4e4
Fogo Adepitan <Fogoadepitan100@gmail.com>
2023-12-03 15:37:17 +0100

    sign-in functionality

--- models/user.js
+++ models/user.js
@@ -1,17 +1,17 @@
 import { Schema, model, models } from "mongoose";
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
   email: {
     type: String,
-    unique: [true, "Email already exists"],
-    required: [true, "Email is required"],
+    unique: [true, "Email already exists!"],
+    required: [true, "Email is required!"],
   },
   username: {
     type: String,
-    required: [true, "Email is required"],
+    required: [true, "Username is required!"],
     match: [
       /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-      "Username invalid,it should contain 8-20 alphanumeric letters and be unique!",
+      "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
     ],
   },
   image: {
@@ -19,6 +19,6 @@ const userSchema = new Schema({
   },
 });
 
-const User = models.User || model("User", userSchema);
+const User = models.User || model("User", UserSchema);
 
 export default User;
