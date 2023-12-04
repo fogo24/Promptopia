e522f1a5e2808d6f1c86efc497bc461d7baff33c
Fogo Adepitan <Fogoadepitan100@gmail.com>
2023-12-04 14:16:05 +0100

    Profile section

---
+++ app/profile/page.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const handleEdit = () => {};
+
+  const handleDelete = async () => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized page"
+      data={[]}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
